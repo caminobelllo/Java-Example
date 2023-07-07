eeb4c234f2dea454a2c592bff93c9e785833edf7
caminobelllo <alstpgml0812@gmail.com>
2023-07-07 18:31:46 +0900

    자바 코드로 스프링 빈 설정

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,6 +5,7 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class MemberController {
     // New로 생성하는 것 대신에 스프링 컨테이너에 등록해서 가져다 쓸 수 있도록 !!
