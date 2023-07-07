894ed8a1c68c36a686c2380fd483d54a63873293
caminobelllo <alstpgml0812@gmail.com>
2023-07-07 18:15:33 +0900

    스프링 빈 등록

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    // New로 생성하는 것 대신에 스프링 컨테이너에 등록해서 가져다 쓸 수 있도록 !!
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
